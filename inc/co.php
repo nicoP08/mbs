c76c4077378d341d624f0bbae627cb2bb3b1b0f4
Inseri <inseri.sama@gmail.com>
2018-09-10 09:16:39 +0200

    maj

---
+++ inc/co.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start()
+
+/*$host_name = 'db752756822.db.1and1.com';
+$database = 'db752756822';
+$user_name = 'dbo752756822';
+$password = 'lR*lBKBO2A)}o9P';
+
+$dbh = null;
+try {
+  $dbh = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
+} catch (PDOException $e) {
+  echo "Erreur!: " . $e->getMessage() . "<br/>";
+  die();
+}*/
+?>
